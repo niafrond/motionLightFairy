6ec52515f5e776ae5323a33c5db7bdbd8a71f748
Mickael <fontaine.mickael@ymail.com>
2016-02-28 01:02:24 +0100

    fake tread with bonfire

--- motionLightFairy/motionLightFairy.ino
+++ motionLightFairy/motionLightFairy.ino
@@ -1,16 +1,14 @@
 int gardenPins[] = {9,10};
-int deerPin[] = {11};
+
 int motionSensorPin = 2;
-int rgbPin[] = {7,8,9};
-int redSat = 0;
-int grennSat = 0;
-int blueSat = 0;
-const int maxLuminosity = 200;
+int rgbPin[] = {3,11,13};
+const int maxLuminosity = 255;
+const int minLuminosity=maxLuminosity/12;
 volatile bool hasMotion = 0;
 
-const long minMotionDuration = 1000;
-volatile long lastMotion = minMotionDuration;
-
+const long minMotionDuration = 4000;
+volatile long lastMotion = 0;
+String serialString = "";
 void setup() {
   // put your setup code here, to run once:
   pinMode(motionSensorPin,INPUT);
@@ -19,21 +17,33 @@ void setup() {
   // different seed numbers each time the sketch runs.
   // randomSeed() will then shuffle the random function.
   randomSeed(analogRead(0));
-  Serial.begin(9600);
+ 
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+   Serial.begin(9600);
   attachInterrupt(digitalPinToInterrupt(motionSensorPin), setMotion, CHANGE);
   hasMotion = digitalRead(motionSensorPin)!=0;
 }
+void debug(String message){
+  Serial.println(message);
+}
 void setMotion(){
-  Serial.println("setMotion");
+  volatile bool inWindow = false;
+  
+  serialString="setMotion : inwindow =>";
+  
   hasMotion = digitalRead(motionSensorPin)!=0;
   int tmpLastMotion = millis();
   if(lastMotion < tmpLastMotion - minMotionDuration){//Si le mouvement a été fait aprés la fenetre d'allumage minimum
     lastMotion = millis();
+    inWindow=true;
   }
   
+  serialString.concat(inWindow);
+  debug(serialString);
+
   
-  Serial.println(lastMotion);
-  Serial.println("setMotion ended");
 }
 
 void sleep(int duration){
@@ -42,39 +52,45 @@ void sleep(int duration){
 void loop() {
   // put your main code here, to run repeatedly
   //test();
-  
+  debug("loop : Full light");
+  //Full light for 5sec
+    analogWrite(gardenPins[0],maxLuminosity);
+    analogWrite(gardenPins[1],maxLuminosity);
+    bonfire(minMotionDuration);
+  debug("loop : Debut de captation");
   while(motion()){
-    analogWrite(deerPin[0],255);
+     
     lightUp(0);
     lightUp(1);
        
   }
+  debug("loop :debut stabilisation");
   for(int sat=2;sat<50;sat++){
       if(motion()){
         return;
       }
+
       analogWrite(gardenPins[0],sat);
       analogWrite(gardenPins[1],sat);
   }
-  sleep(10000);
-  //On commence l'extinction
+  debug("loop:big wait");
+  bonfire(10000);
+   debug("loop:On commence l'extinction");
   for(int sat=50;sat>2;sat=sat-1){
       if(motion()){
         return;
       }
-       //rgb(0,sat,sat);
-      analogWrite(deerPin[0],sat);
       analogWrite(gardenPins[0],sat);
       analogWrite(gardenPins[1],sat);
-      sleep(1000);
+      bonfire(1000);
     }
 
-  
+  debug("loop:Pas de mouvement");
   while(!motion()){
-    analogWrite(deerPin[0],0);
+    rgb(0,0,0);
     analogWrite(gardenPins[0],0);
     analogWrite(gardenPins[1],0);
-    sleep(2000);
+    sleep(500);
     
   }
 
@@ -82,19 +98,23 @@ void loop() {
 }
 
 bool motion(){
-  Serial.println("motion");
-  Serial.println(millis());
+
   if(hasMotion || lastMotion > millis()-minMotionDuration){
-    Serial.println("yes motion");
+    serialString="yes motion => ";
+    if(hasMotion){
+        serialString.concat("True motion");
+    }
+    else{
+      serialString.concat("Timer");
+    }
+    debug(serialString);
     return true;
   }else{
-    Serial.println("no motion");
+   
     return false;
   }
- 
   
 }
-
 
 void rgb(int r,int g,int b){
     
@@ -105,28 +125,49 @@ void rgb(int r,int g,int b){
 }
 void lightUp(int pin){
     int pinNormal = !pin;
-    //Disable the interrupt during the light is changing
+    
+    //Disable the interrupt while the light is changing
     detachInterrupt(digitalPinToInterrupt(motionSensorPin));
-    analogWrite(gardenPins[pinNormal],2);
-    for(int sat=2;sat<maxLuminosity;sat=sat+2){
-
-       
-       //rgb(0,sat,sat);
+    Serial.println("lightUp: On commence par baisser les lumiéres");
+    for(int sat=maxLuminosity;sat>minLuminosity;sat=sat-5){
       analogWrite(gardenPins[pin],sat);
-      sleep(100);
+      analogWrite(gardenPins[pinNormal],sat);
+      bonfire(100);
     }
-    for(int sat=maxLuminosity;sat>0;sat=sat-2){
+    analogWrite(gardenPins[pinNormal],minLuminosity);
+    Serial.println("lightUp : Degrade UP");
+    for(int sat=minLuminosity;sat<maxLuminosity;sat=sat+2){
 
-      
-       //rgb(0,sat,sat);
       analogWrite(gardenPins[pin],sat);
-      sleep(100);
+      bonfire(100);
     }
-    analogWrite(gardenPins[pin],2);
+    Serial.println("lightUp : Degrade Down");
+    for(int sat=maxLuminosity;sat>minLuminosity;sat=sat-2){
+      analogWrite(gardenPins[pin],sat);
+      bonfire(100);
+    }
+    analogWrite(gardenPins[pin],minLuminosity);
     //reenable the interrupt
     attachInterrupt(digitalPinToInterrupt(motionSensorPin), setMotion, CHANGE);
-    sleep(1000);
+    bonfire(1000);
 }
+//We maintain a bonfire while sleeping
+void bonfire(unsigned long sleepDuration){
+  serialString="bonfire start for ";
+  long startTime = millis();
+  serialString.concat(sleepDuration);
+  //debug(serialString);
+
+  while(startTime > (millis()-sleepDuration)){
+   
+
+    int green = random(55);
+    int red = constrain(green*5,0,255);//To make orange, red need o be 5 times greater
+    rgb(red,green,0);
+    sleep(random(100));
+  }
+}
+
 
 
 
